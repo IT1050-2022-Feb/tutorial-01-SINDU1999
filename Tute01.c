39957518aa532bb06449a492f9f1df1b32acace1
IT21158568 <99710501+it21158568@users.noreply.github.com>
2022-02-15 11:44:44 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,19 @@
 
 #include <stdio.h>
 
-int main() {
-  
-  return 0;
+int main() 
+{
+   int sum,subject_one_mark,subject_two_mark;//declare variables
+   float average;//declare variables
+   printf("Enter the SUBJECT ONE MARK :");//display massege
+   scanf("%d",&subject_one_mark);//get subject one mark
+   printf("Enter the SUBJECT TWO MARK :");//display massege
+   scanf("%d",&subject_two_mark);//get subject two mark
+   
+   sum=subject_one_mark+subject_two_mark;//get total of two marks
+   average=sum/2;//get average
+   
+   printf("The average is : %.2f",average);//print average
+   return 0;
 }
 
